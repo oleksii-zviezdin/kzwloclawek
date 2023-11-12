ba432980f73328b0ef7bcd28aad92be2bdb9844e
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-11-12 23:26:17 +0100

    create base for project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
+import { About, Home, SupportUs, WhoWeAre } from 'pages/index';
+import { Acts, Beliefs, History } from 'components';
+import { Routes, Route } from 'react-router-dom';
+import { Container, Header, Logo, Link } from './App.styled';
+
+import logo from '../img/logo.png';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <Logo>
+          <img src={logo} width="50px" alt="logo" />
+          <div>
+            <p>Zbór Chrystusa Zbawiciela</p>
+            <p style={{ fontSize: '12px' }}>
+              Kościół Zielonoświątkowy we Włocławku
+            </p>
+          </div>
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Strona Główna
+          </Link>
+          <Link to="/about">O nas</Link>
+          <Link to="/who-we-are">Kim jesteśmy</Link>
+          <Link to="/support-us">Wesprzyj nas</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="our-beliefs" element={<Beliefs />} />
+          <Route path="history" element={<History />} />
+          <Route path="legal-acts" element={<Acts />} />
+        </Route>
+        <Route path="/who-we-are" element={<WhoWeAre />} />
+        <Route path="/support-us" element={<SupportUs />} />
+      </Routes>
+    </Container>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <img src="" alt="" />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
+        voluptatum voluptate.
+      </p>
+    </main>
+  );
+};
+
+export default Home;
