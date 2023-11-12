4f5e8b3948f954222fc19bcf8de0f03dada41337
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-11-13 00:21:51 +0100

    step to fix

---
+++ src/components/Acts/Acts.jsx
@@ -0,0 +1,47 @@
+const Acts = () => {
+  return (
+    <main>
+      <h1>Acty Prawne</h1>
+      <ul>
+        <li>
+          <a href="https://kz.pl/kzw/wp-content/uploads/ust_41_1997.pdf">
+            W Ustawa z dnia 20 lutego 1997 r. o stosunku Państwa do Kościoła
+            Zielonoświątkowego w Rzeczpospolitej Polskiej (Dz.U. 1997 nr 41 poz.
+            254) wierzymy
+          </a>
+        </li>
+        <li>
+          <a href="https://kz.pl/kzw/wp-content/uploads/ust_128_1997_832.pdf">
+            Rozporządzenie Ministra Edukacji Narodowej oraz Ministra Zdrowia i
+            Opieki Społecznej (Dz. U. 1997 nr 128, poz. 832)
+          </a>
+        </li>
+        <li>
+          <a href="https://kz.pl/kzw/wp-content/uploads/ust_67_1999_753.pdf">
+            Rozporządzenie Ministra Edukacji Narodowej z dnia 30 czerwca 1999 r.
+            zmieniające rozporządzenie w sprawie warunków i sposobu
+            organizowania nauki religii w szkołach publicznych (Dz.U. 1999 Nr 67
+            poz. 753)
+          </a>
+        </li>
+        <li>
+          <a href="https://kz.pl/kzw/wp-content/uploads/ust_36_1992_155.pdf">
+            Rozporządzenie Ministra Edukacji Narodowej z dnia 14 kwietnia 1992
+            r. w sprawie warunków i sposobu organizowania nauki religii w
+            szkołach publicznych (Dz. U. 1992, Nr 36, poz. 155)
+          </a>
+        </li>
+        <li>
+          <a href="https://kz.pl/kzw/wp-content/uploads/DU20031591546.pdf">
+            Rozporządzenie Ministra Sprawiedliwości z dnia 2 września 2003 r. w
+            sprawie szczegółowych zasad wykonywania praktyk religijnych i
+            korzystania z posług religijnych w zakładach karnych i aresztach
+            śledczych (Dz. U. 2003, Nr 159, poz. 1546)
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default Acts;
---
+++ src/components/Beliefs/Beliefs.jsx
@@ -0,0 +1,12 @@
+const Beliefs = () => {
+  return (
+    <main>
+      <h1>W co wierzymy</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </main>
+  );
+};
+
+export default Beliefs;
