6211477c6d6acd52d4c68c8cabacf67be805a369
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-11-13 00:19:13 +0100

    test to fix

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as About } from './About';
-export { default as Home } from './Home';
-export { default as WhoWeAre } from './WhoWeAre';
-export { default as SupportUs } from './SupportUs';
