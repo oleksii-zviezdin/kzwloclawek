f138cca08d408b4d0b982e4a1fa11cb74139b365
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-11-27 23:27:07 +0100

    update BeliefsData - nr6 to nr 11

--- src/components/Beliefs/BeliefsData.js
+++ src/components/Beliefs/BeliefsData.js
@@ -2,8 +2,9 @@ const beliefsData = [
   {
     nb: '1',
     title: 'Pismo Święte',
-    content:
+    content: [
       'Pismo Święte jest w pełni wystarczającą, pewną nieomylną normą poznania, wiary i posłuszeństwa, które stanowią o zbawieniu. Jest ono w całości natchnionym przez Ducha Świętego Słowem Bożym. Dlatego oświecenie przez Ducha Świętego jest konieczne do jego właściwego zrozumienia. Pismo Święte, czyli zapisane Słowo Boże zawiera następujące księgi, które dzielą się na Stary i Nowy Testament:',
+    ],
     subTitle1: 'Stary Testament',
     subTitle2: 'Nowy Testament',
     subContent1:
@@ -30,8 +31,9 @@ const beliefsData = [
   {
     nb: '2',
     title: 'Bóg Trójjedyny',
-    content:
+    content: [
       'Jest tylko jeden żywy i prawdziwy Bóg. Jest On samoistny i nieskończony w swojej istocie i doskonałości. Tylko On posiada nieśmiertelność i przebywa w światłości niedostępnej. Bóg stworzył niebo i ziemie, świat ludzi i aniołów. Bóg jedyny objawił się jako Byt w trzech osobach – Ojca i Syna, i Ducha Świętego – które stanowią jedność w istocie, mocy i wieczności.',
+    ],
     reference: {
       referenceTitle: 'Referencje biblijne',
       referenceContent: [
@@ -63,8 +65,9 @@ const beliefsData = [
   {
     nb: '3',
     title: 'Jezus Chrystus',
-    content:
+    content: [
       'Jezus Chrystus jest jednorodzonym Synem Bożym – odwiecznym i wcielonym Słowem Boga – który się począł z Ducha Świętego i narodził się z Marii Dziewicy. Posiada dwie natury – boską i ludzką, jest Synem Bożym i Synem Człowieczym. On poniósł śmierć na krzyżu za grzech świata i zmartwychwstał w ciele dla usprawiedliwienia wierzących. Po zmartwychwstaniu Jezus Chrystus wstąpił do nieba i zasiadł na prawicy Ojca, skąd przyjdzie powtórnie. Jezus Chrystus jest jedynym pośrednikiem miedzy Bogiem a człowiekiem.',
+    ],
     reference: {
       referenceTitle: 'Referencje biblijne',
       referenceContent: [
@@ -113,8 +116,9 @@ const beliefsData = [
   {
     nb: '4',
     title: 'Upadek i zbawienie człowieka',
-    content:
+    content: [
       'Człowiek został stworzony przez Boga jako istota prawa i czysta. Przez swój grzech utracił pierwotną sprawiedliwość i jego szczęśliwą społeczność z Bogiem została przerwana. Grzech pierwszego człowieka – jako mężczyzny i kobiety – objął wszystkich ludzi i dlatego śmierć stała się udziałem wszystkich. Zbawienie dokonało się przez śmierć Jezusa Chrystusa, Baranka Bożego na krzyżu i jest dostępne każdemu człowiekowi z łaski Bożej przez wiarę. Skutkiem zbawczej wiary jest usprawiedliwienie, odrodzenie, usynowienie i uświęcenie. Zbawczej wierze towarzyszą dobre uczynki.',
+    ],
     reference: {
       referenceTitle: 'Referencje biblijne',
       referenceContent: [
@@ -137,8 +141,9 @@ const beliefsData = [
   {
     nb: '5',
     title: 'Duch Święty',
-    content:
+    content: [
       'Duch Święty jest trzecią, boską osobą Trójcy Świętej. Z Jego natchnienia powstało Pismo Święte. Z Ducha Świętego począł się Jezus Chrystus, który również “przez Ducha wiecznego ofiarował samego siebie bez skazy Bogu”. Duch Święty udziela się człowiekowi w odrodzeniu, chrzcie w Duchu, napełnieniu oraz w duchowych darach łaski Bożej.',
+    ],
     reference: {
       referenceTitle: 'Referencje biblijne',
       referenceContent: [
@@ -159,6 +164,178 @@ const beliefsData = [
         { contentTitle: 'Ga', contetn: ['4:6'] },
         { contentTitle: '1-P', contetn: ['1:11', '4:16'] },
         { contentTitle: 'Ap', contetn: ['22:17'] },
+      ],
+    },
+  },
+  {
+    nb: '6',
+    title: 'Chrzest Duchem Świętym',
+    content: [
+      'Chrzest Duchem Świętym jest wyzwoleniem Ducha Świętego w wierzących, wzmocnieniem i udzieleniem duchowych darów łaski, charyzmatów, ku budowaniu Ciała Chrystusowego – Kościoła, dla skutecznej służby i świadectwa. Znakiem chrztu Duchem Świętym jest przemienione życie, duchowe dary łaski Bożej, w tym natchnione przez Ducha Świętego, mówienie innymi językami (glosolalia).',
+    ],
+    reference: {
+      referenceTitle: 'Referencje biblijne',
+      referenceContent: [
+        { contentTitle: 'Mt', contetn: ['3:11-12'] },
+        { contentTitle: 'Mk', contetn: ['1:8'] },
+        { contentTitle: 'Łk', contetn: ['3:16', '12:49-50'] },
+
+        { contentTitle: 'J', contetn: ['1:33'] },
+        {
+          contentTitle: 'Dz',
+          contetn: ['1:5', '2:4,17,38-39', '8:14-17', '10:44', '19:,6'],
+        },
+        { contentTitle: '1-Kor', contetn: ['12:14'] },
+
+        { contentTitle: 'Ef', contetn: ['4:4-16'] },
+      ],
+    },
+  },
+  {
+    nb: '7',
+    title: 'Ustanowienia Chrystusowe',
+    content: [
+      'Chrystusowe ustanowienia dla Kościoła to – Chrzest i Wieczerza Pańska. Chrzest w Kościele jest chrztem wiary. Jest on znakiem wspólnoty z Chrystusem w Jego śmierci i zmartwychwstaniu, znakiem wszczepienia w Chrystusa i przebaczenia grzechów. Chrzest jest wykonywany w imię Ojca i Syna, i Ducha Świętego przez zanurzenie w wodzie. Wieczerza Pańska jest wspominaniem, pamiątki śmierci Pańskiej; obchodzona jest pod dwiema postaciami: chleba i wina. Chleb jest “społecznością ciała Chrystusowego”, kielich jest “społecznością krwi Chrystusowej”. Obecność Chrystusa w Wieczerzy Pańskiej jest rzeczywista, ale duchowa, nie materialna.',
+    ],
+    reference: {
+      referenceTitle: 'Referencje biblijne',
+      referenceContent: [
+        { contentTitle: 'Mt', contetn: ['3:16', '28:18-20'] },
+        { contentTitle: 'Mk', contetn: ['16:15-16'] },
+        { contentTitle: 'Dz', contetn: ['2:38, 41', '9:10-18'] },
+
+        { contentTitle: 'Rz', contetn: ['6:3-10'] },
+        { contentTitle: 'Rdz', contetn: ['12'] },
+        { contentTitle: 'Mt', contetn: ['26:17-30'] },
+
+        { contentTitle: 'Mk', contetn: ['14:12-26'] },
+
+        { contentTitle: 'Łk', contetn: ['22:7-23', '24:29-31'] },
+        { contentTitle: 'J', contetn: ['13:21-30'] },
+        { contentTitle: '1-Kor', contetn: ['10:16-21', '11:23-34'] },
+      ],
+    },
+  },
+  {
+    nb: '8',
+    title: 'Uzdrowienie chorych',
+    content:
+      'Fizyczne ciało człowieka zostanie odkupione dopiero w chwili powtórnego, chwalebnego przyjścia Jezusa Chrystusa przez zmartwychwstanie umarłych i przemienienie żyjących, kiedy “to, co skazitelne przyoblecze się w nieskazitelność”. Bóg jednakże okazuje łaskę uzdrowienia również w doczesności przez modlitwę wiary w zbawcze dzieło Jezusa Chrystusa, dary uzdrawiania oraz przez nauki medyczne.',
+    reference: {
+      referenceTitle: 'Referencje biblijne',
+      referenceContent: [
+        { contentTitle: 'Wj', contetn: ['15:26'] },
+        { contentTitle: 'Lb', contetn: ['12:10-16', '21:4-10'] },
+        { contentTitle: '2-Krl', contetn: ['21:1-11'] },
+        { contentTitle: 'Iz', contetn: ['38:1-8', '53:4-5'] },
+        { contentTitle: 'Mt', contetn: ['8:1-17', '9:12', '10:1,8'] },
+        { contentTitle: 'Mk', contetn: ['2:17', '16:17-18,20'] },
+        { contentTitle: 'J', contetn: ['5:31'] },
+        { contentTitle: 'Dz', contetn: ['3:1-9', '5:12-16', '10:32-35'] },
+        { contentTitle: '1-Kor', contetn: ['12:9,28'] },
+        { contentTitle: '1-Tm', contetn: ['5:23'] },
+        { contentTitle: '2-Tm', contetn: ['4:20'] },
+        { contentTitle: 'Jk', contetn: ['5:13-16'] },
+      ],
+    },
+  },
+  {
+    nb: '9',
+    title: 'Kościół',
+    content: [
+      'Założycielem Kościoła jest Jezus Chrystus. Do Kościoła Powszechnego należą ludzie wierzący i odrodzeni z Ducha Świętego zarówno żyjący na ziemi, jak i ci, którzy “zasnęli w Chrystusie”. Ci, którzy odeszli do wieczności stanowią Kościół Tryumfujący, zaś jeszcze żyjący na ziemi – Kościół Pielgrzymujący. Głową Kościoła jest Jezus Chrystus.',
+      'Kościół lokalny nazywa się zborem. Zbory mogą łączyć się we wspólnoty kościelne, denominacje. Do budowania Kościoła, Ciała Chrystusowego, Pan ustanowił odpowiednie służby, np. biskupów, prezbiterów, pasterzy, nauczycieli, ewangelistów, diakonów.',
+    ],
+    reference: {
+      referenceTitle: 'Referencje biblijne',
+      referenceContent: [
+        { contentTitle: 'Wj', contetn: ['15:26'] },
+        { contentTitle: 'Lb', contetn: ['12:10-16', '21:4-10'] },
+        { contentTitle: '2-Krl', contetn: ['21:1-11'] },
+        { contentTitle: 'Iz', contetn: ['38:1-8', '53:4-5'] },
+        { contentTitle: 'Mt', contetn: ['8:1-17', '9:12', '10:1,8'] },
+        { contentTitle: 'Mk', contetn: ['2:17', '16:17-18,20'] },
+        { contentTitle: 'J', contetn: ['5:31'] },
+        { contentTitle: 'Dz', contetn: ['3:1-9', '5:12-16', '10:32-35'] },
+        { contentTitle: '1-Kor', contetn: ['12:9,28'] },
+        { contentTitle: '1-Tm', contetn: ['5:23'] },
+        { contentTitle: '2-Tm', contetn: ['4:20'] },
+        { contentTitle: 'Jk', contetn: ['5:13-16'] },
+      ],
+    },
+  },
+  {
+    nb: '10',
+    title: 'Małżeństwo',
+    content: [
+      'Małżeństwo zostało ustanowione przez Boga w akcie stworzenia. I może ono być zawarte tylko miedzy jednym mężczyzną i jedną kobietą. Nie może być zawarte miedzy bliskimi krewnymi, ponieważ jest to zabronione przez Słowo Boże. Celem małżeństwa jest zarówno prokreacja, przekazywanie daru życia następnemu pokoleniu, jak i wspólnota oraz pomoc wzajemna. Zasadą małżeństwa jest jego nierozerwalność.',
+    ],
+    reference: {
+      referenceTitle: 'Referencje biblijne',
+      referenceContent: [
+        { contentTitle: 'Rdz', contetn: ['1;26-28', '2:20-25', '24:1-67'] },
+
+        { contentTitle: 'Ne', contetn: ['13:25-27'] },
+
+        { contentTitle: 'Mt', contetn: ['19:3-12'] },
+
+        { contentTitle: 'Mk', contetn: ['10:1-12'] },
+
+        { contentTitle: 'Ef', contetn: ['5:21-32'] },
+
+        { contentTitle: '1-Kor', contetn: ['7:2-40'] },
+
+        { contentTitle: '1-Tm', contetn: ['3:2,12', '4:3'] },
+
+        { contentTitle: 'Hbr', contetn: ['13:4'] },
+      ],
+    },
+  },
+  {
+    nb: '11',
+    title: 'Rzeczy ostateczne',
+    content: [
+      'Bóg wyznaczył dzień, w którym będzie sądził świat sprawiedliwie przez Jezusa Chrystusa. W dniu tym będą sądzeni wszyscy ludzie oraz upadli aniołowie z szatanem na czele. Nastąpi wówczas powszechne zmartwychwstanie: sprawiedliwych w przemienionym ciele do życia wiecznego, niesprawiedliwych zaś – na wieczne potępienie. Dziedzictwem sprawiedliwych będzie “nowe niebo i nowa ziemia, w których sprawiedliwość mieszka”, zaś niesprawiedliwych – “jezioro ogniste”.',
+    ],
+    reference: {
+      referenceTitle: 'Referencje biblijne',
+      referenceContent: [
+        { contentTitle: 'Rdz', contetn: ['3:19'] },
+
+        { contentTitle: 'Wj', contetn: ['3:1-6'] },
+
+        { contentTitle: 'Kzn', contetn: ['12:7'] },
+
+        { contentTitle: 'Hi', contetn: ['19:25-27'] },
+
+        { contentTitle: 'Mt', contetn: ['24:1-44', '25:1-13, 31-46'] },
+
+        { contentTitle: 'Łk', contetn: ['16:19-31', '20:27-38'] },
+
+        { contentTitle: 'J', contetn: ['11:23-27, 32-44;'] },
+
+        { contentTitle: 'Dz', contetn: ['2:29-32', '13:36-39'] },
+
+        { contentTitle: 'Rz', contetn: ['1:1-4', '14:7-9'] },
+
+        { contentTitle: '1-Kor', contetn: ['3:10-17', '15:1-55'] },
+
+        { contentTitle: '2-Kor', contetn: ['5:1-10'] },
+
+        { contentTitle: 'Flp', contetn: ['2:9'] },
+
+        { contentTitle: '1-Tes', contetn: ['4:13-18', '5:1-11'] },
+
+        { contentTitle: '2-Tes', contetn: ['1:6-10', '2:1-12'] },
+
+        { contentTitle: 'Hbr', contetn: ['13:8'] },
+
+        { contentTitle: '1-P', contetn: ['1:10-12'] },
+
+        { contentTitle: '2-P', contetn: ['2:4-13', '3:1-13'] },
+
+        { contentTitle: 'Jud', contetn: ['1:6-9, 14-15'] },
+        { contentTitle: 'Ap', contetn: ['20-22'] },
       ],
     },
   },
